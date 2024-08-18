db1de0ca55b39e2cf4a7f5f95741c6ca2a2717ec
amengdv <abeamin23@gmail.com>
2024-08-18 12:38:20 +0800

    get table data basic

---
+++ html_parser/parser.js
@@ -0,0 +1,47 @@
+import { JSDOM } from 'jsdom'
+
+function getTableData(htmlBody) {
+    const dom = new JSDOM(htmlBody);
+
+    const ths = dom.window.document.getElementsByTagName('th');
+    const tds = dom.window.document.getElementsByTagName('td');
+
+    // Get content inside th
+    const thData = [];
+    for (const th of ths) {
+        thData.push(th.textContent);
+    }
+
+    // Get content inside td
+    const tdData = [];
+    for (const td of tds) {
+        if (td.querySelector('a') !== null) {
+            const anchorData = [];
+            const anchorHref = td.querySelector('a').getAttribute('href');
+            const anchorContent = td.querySelector('a').textContent;
+            anchorData.push(anchorHref);
+            anchorData.push(anchorContent);
+            tdData.push(anchorData);
+        } else {
+            tdData.push(td.textContent);
+        }
+    }
+
+
+    const tableDatas = []
+
+    for (let i = 0; i < tdData.length; i += thData.length) {
+        const tableData = {};
+        for (let j = 0; j < thData.length; j++) {
+            tableData[thData[j]] = tdData[j + i];
+        }
+        tableDatas.push(tableData)
+    }
+
+    return tableDatas
+
+}
+
+export {
+    getTableData
+};
