5635f93a6c3e531114dcb778aeb303ec65f10682
amengdv <abeamin23@gmail.com>
2024-08-20 00:27:12 +0800

    finish basic functionality

--- data_handler/filter.js
+++ data_handler/filter.js
@@ -1,3 +1,7 @@
+import { fetchHtml, getContentWithDataType } from '../html_parser/parser.js'
+import pLimit from 'p-limit'
+const limit = pLimit(5)
+
 function filterTownhall(data, level) {
     const filtered = []
     for (const obj of data) {
@@ -21,24 +25,71 @@ function filterTownhall(data, level) {
 function filterTrophy(data, trophy) {
     const filtered = []
     for (const obj of data) {
-        const townhallData = obj['Trophies']
-        if (townhallData === 'All') {
+        const trophyData = obj['Trophies'].replace(/,/g, '')
+        if (trophyData === 'All') {
             filtered.push(obj)
-        } else if (townhallData.includes('-')) {
-            const ranges = townhallData.split('-')
-            if (trophy >= ranges[0] && trophy <= ranges[1]) {
+        } else if (trophyData.includes('-')) {
+            const ranges = trophyData.split('-')
+            if (trophy >= Number(ranges[0]) && trophy <= Number(ranges[1])) {
                 filtered.push(obj)
             }         
-        } else if (townhallData <= trophy) {
+        } else if (trophyData.includes('+')) {
+            const num = Number(trophyData.trim('+'))
+            if (num <= trophy) {
+                filtered.push(obj)
+            }
+        } else if (trophyData <= trophy) {
             filtered.push(obj)
+        } else {
+            continue
         }
+        
 
     }
     return filtered
 }
 
-function filterTroops(data) {
-    console.log(data)
+
+async function filterTroops(data, troops=[], baseUrl) {
+    const filtered = []
+    let troopsData = []
+
+    console.log('Fetching HTML and extracting troops data...')
+    console.time()
+    try {
+        troopsData = await Promise.all(
+            data.slice(0, 25).map(obj => limit(async () => {
+                const body = await fetchHtml( new URL(obj.Article[0], baseUrl) ) 
+                return getContentWithDataType(body, "troop type", 'Spells')
+            }))
+        )
+
+    } catch(err) {
+        console.log(err.message)
+    }
+    console.timeEnd()
+    console.log(`Done fetching HTML and extracting troops data`)
+
+
+    console.log('Starting filtering')
+    troopsData.forEach((troopData, index) => {
+        const troopSet = new Set(troopData)
+        const isSubset = troops.every(troop => {
+            return troopSet.has(troop)
+        })
+
+        // console.log(troopSet)
+        // console.log(isSubset)
+
+        if (isSubset) {
+            console.log('pushing to filtered')
+            const dataObj = data[index]
+            filtered.push(dataObj)
+        }
+    })
+
+    console.log('returning filtered')
+    return filtered
 }
 
 export {
---
+++ main.sh
@@ -0,0 +1,1 @@
+node --no-deprecation "/home/aminkafri/workspace/node/amengdv/coc-attack-strats/main.js" "$@"
